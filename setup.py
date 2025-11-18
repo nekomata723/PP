6d3d9222ea8a2a355113a026027ac669eb9fb893
nekomata723 <karuego723@gmail.com>
2025-11-18 12:25:31 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PayPaython_mobile",
+    version="1.0.0",
+    packages=find_packages(),
+    description="PayPaython mobile fork",
+    author="nekomata723",
+)
