21c34ab8a6069127a4f7eb4b996178898e0ae7c4
nekomata723 <karuego723@gmail.com>
2025-11-26 08:30:15 +0900

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="PayPaython_mobile",
-    version="1.0.0",
-    packages=find_packages(),
-    description="PayPaython mobile fork",
-    author="nekomata723",
-)
